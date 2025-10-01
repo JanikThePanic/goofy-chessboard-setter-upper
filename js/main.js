b470363072e22c1e4599eb34b7bc4d7283f7285c
janik <jk4abdl@gmail.com>
2025-10-01 16:19:59 -0400

    setup

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*
+i think the play is, we set up a chess board like normal
+then we just random of times iterate through random pieces - lets say x turns (turn being 1 sides action)
+and then we randomly select a piece (lets say piece-y) and then we check all possible legal moves for piece-y
+and then we randomly select one of those legal moves and move piece-y to that location
+
+if piece-y can not move, we reselect another piece-y and try again
+
+and then we repeat this x times
+
+caviats:
+need to make sure niether kings are in mate... or check... or stalemate
+ig if no piece-y can legally move thats a stalemate... so we can end it there...
+
+ill make it up as i go
+
+*/
+
